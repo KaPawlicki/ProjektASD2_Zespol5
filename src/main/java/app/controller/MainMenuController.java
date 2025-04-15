a040c1d3fe848bc2328ad319da029af3bf16800e
kacper <kacperpawlicki@mat.umk.pl>
2025-04-15 21:45:10 +0200

    Zmieniono sposób zarządzania instancją ShireMap – trzymana w SceneManagerze i przekazywana przez konstruktor do kontrolerów

--- src/main/java/app/controller/MainMenuController.java
+++ src/main/java/app/controller/MainMenuController.java
@@ -1,10 +1,15 @@
 package app.controller;
 
+import app.model.Field;
+import app.model.ShireMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
+import java.awt.*;
 
 public class MainMenuController {
+
+    private ShireMap shireMap;
     @FXML
     private Button startButton;
     @FXML
@@ -14,6 +19,9 @@ public class MainMenuController {
     @FXML
     private Button fromPastScenarioButton;
 
+    public MainMenuController(ShireMap shireMap) {
+        this.shireMap = shireMap;
+    }
 
     @FXML
     public void initialize() {
@@ -30,5 +38,15 @@ public class MainMenuController {
         fromPastScenarioButton.setOnAction(event -> {
             startButton.setDisable(false);
         });
+
+//        startButton.setOnAction(event -> {
+//            Field field1 = new Field("Field1", new Point(0, 0), 20);
+//            Field field2 = new Field("Field2", new Point(10, 0), 30);
+//            shireMap.addNode(field1);
+//            shireMap.addNode(field2);
+//
+//            shireMap.print();
+//
+//        });
     }
 }
--- src/main/java/app/util/SceneManager.java
+++ src/main/java/app/util/SceneManager.java
@@ -1,4 +1,5 @@
 package app.util;
+import app.model.ShireMap;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import java.util.Objects;
 public class SceneManager {
 
     private static Stage primaryStage;
+    private static final ShireMap shireMap = new ShireMap(); // glowna wspoldzielona instancja mapy
 
     public static void setStage(Stage stage) {
         primaryStage = stage;
@@ -30,7 +32,19 @@ public class SceneManager {
 
     public static void switchScene(String fxmlPath, String cssPath) {;
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(SceneManager.class.getResource(fxmlPath)));
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(
+                    SceneManager.class.getResource(fxmlPath)));
+
+            //przekazywanie instancji mapy do konstruktora kontrolera
+            loader.setControllerFactory(type -> {
+                try {
+                    return type.getConstructor(ShireMap.class).newInstance(shireMap);
+                } catch (Exception e) {
+                    throw new RuntimeException("Nie można utworzyć kontrolera: " + type, e);
+                }
+            });
+
+            Parent root = loader.load();
             Scene scene = new Scene(root);
             scene.getStylesheets().add(Objects.requireNonNull(
                     SceneManager.class.getResource(cssPath)).toExternalForm());
