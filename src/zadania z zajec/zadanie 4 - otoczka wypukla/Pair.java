49987b0dfa409ac9857f86e79e6294389e780f87
Kacper <kacperpawlicki@mat.umk.pl>
2025-06-11 12:40:05 +0200

    Dodanie zadan z zajec

---
+++ src/zadania z zajec/zadanie 4 - otoczka wypukla/Pair.java
@@ -0,0 +1,21 @@
+public class Pair {
+    int a, b;
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + a + ", " + b + ">";
+    }
+}
